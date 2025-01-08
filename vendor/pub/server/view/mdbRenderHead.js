391a5a968747c6626ab26453124a3ac93a084ebf
flower-br <contact@flow-er.fr>
2025-01-08 12:22:19 +0100

    init

--- vendor/pub/server/view/mdbRenderHead.js
+++ vendor/pub/server/view/mdbRenderHead.js
@@ -19,6 +19,13 @@ const mdbRenderHead = ({ context, entity, view }, data) => {
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
         />
+        <!-- Google Fonts Albert Sans -->
+        <link 
+        rel='stylesheet' 
+        id='gdlr-core-google-font-css' 
+        href='https://fonts.googleapis.com/css?family=Albert+Sans%3A100%2C200%2C300%2Cregular%2C500%2C600%2C700%2C800%2C900%2C100italic%2C200italic%2C300italic%2Citalic%2C500italic%2C600italic%2C700italic%2C800italic%2C900italic%7CAbril+Fatface%3Aregular&#038;subset=latin%2Clatin-ext&#038;ver=6.7.1' 
+        type='text/css' media='all' 
+        />
         <!-- MDB ESSENTIAL -->
         <link rel="stylesheet" href="/mdb/cli/resources/mdb/css/mdb.min.css" />
         <!-- MDB PLUGINS -->
