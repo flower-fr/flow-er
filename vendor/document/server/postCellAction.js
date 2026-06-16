6d09ed9bdf17ea4287724810ec6418b328456ea1
BrankV <victor.chansolme@hotmail.fr>
2026-06-16 16:21:08 +0200

    Support batch insert in postCellAction

--- vendor/document/server/postCellAction.js
+++ vendor/document/server/postCellAction.js
@@ -8,26 +8,31 @@ const postCellAction = async ({ req }, context, { sql, logger }) =>
 
     logger && logger.debug(`postAction called with body: ${util.inspect(req.body)}`)
     
-    checkParams(req, context) // we check that the params are valid
-
-    const data = req.body
+    const items = Array.isArray(req.body) ? req.body : [req.body]
+    for (const item of items) {
+        checkParams({ body: item }, context) // we check that the params are valid
+    }
 
     // we set the state param at active
-    data.is_canceled = 0
-    data.document_id = parseInt(document_id)
-    if (!data.identifier) {
-        data.identifier = null
+    for (const data of items) {
+        data.is_canceled = 0
+        data.document_id = parseInt(document_id)
+        if (!data.identifier) {
+            data.identifier = null
+        }
     }
 
     try {
         await sql.beginTransaction()
 
-        const id = await sql.execute({ context, type: "insert", entity: "document_cell", data })
-        if (!data.identifier) {
-            await sql.execute({ context, type: "update", entity: "document_cell", ids: [id], data: { identifier: id } })
+        for (const data of items) {
+            const id = await sql.execute({ context, type: "insert", entity: "document_cell", data })
+            if (!data.identifier) {
+                await sql.execute({ context, type: "update", entity: "document_cell", ids: [id], data: { identifier: id } })
+            }
         }
         await sql.commit()
-        return JSON.stringify({ response: "cellule insérée avec succès" })
+        return JSON.stringify({ response: `${items.length} cellule insérée avec succès` })
 
     } catch (err) {
         logger && logger.debug(util.inspect(err))
