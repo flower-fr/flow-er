e50d396a65de0b3f21ebd5a493b0e8cd6835e2de
BrankV <victor.chansolme@hotmail.fr>
2026-08-03 16:44:59 +0200

    V260731

--- vendor/bo/client/controller/load.js
+++ vendor/bo/client/controller/load.js
@@ -1,11 +1,11 @@
 import Controller from "./Controller.js"
 import Layout from "../view/layout/Layout.js"
 
-const [application, tab, entity, view, theme, locale] = Object.values(JSON.parse(decodeURI(document.getElementById("module-script").dataset.json)))
+const [application, tab, entity, view, theme, locale, profileId] = Object.values(JSON.parse(decodeURI(document.getElementById("module-script").dataset.json)))
 
 const init = async () => {
     const controller = new Controller({ url: `/bo/index/${ application }/${ tab }?entity=${ entity }` })
-    const layout = new Layout({ controller, application, tab, entity, view, locale, theme })
+    const layout = new Layout({ controller, application, tab, entity, view, locale, theme, profileId })
     await layout.initialize()
     $("body").html(controller.render(layout))
     controller.trigger(layout)
