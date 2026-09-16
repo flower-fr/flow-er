8bf4ec9f5fb80d298e98887481a6bdd2b7596bb9
BrankV <victor.chansolme@hotmail.fr>
2026-09-16 12:28:28 +0200

    V260925

--- vendor/bo/config/model/guided_action.js
+++ vendor/bo/config/model/guided_action.js
@@ -23,14 +23,23 @@ module.exports = {
             column: "profile_id",
             type: "integer"
         },
+        validity_date: {
+            entity: "guided_action",
+            type: "date",
+            column: "validity_date"
+        },
         identifier: {
             column: "identifier",
             type: "CONCAT",
-            components: ["entity", "view", "profile_id"]
+            components: ["entity", "view", "profile_id", "validity_date"]
         },
         status: {
             entity: "guided_action",
             column: "status"
+        },
+        where_ids: {
+            entity: "guided_action",
+            column: "where_ids"
         },
     },
     audit: "audit"
